45dede6a0be779f6c9faedfc8d9dd5bde31e91a5
Lance Arsenault <lanceman@vt.edu>
2019-02-28 23:14:44 -0500

    Saving Work

--- htdocs/scope/scope.js
+++ htdocs/scope/scope.js
@@ -39,12 +39,35 @@ function _GetGridSpacing(pixPerGrid, min, max, pixels/*width or height*/) {
 
     let b = Math.ceil(delta/upow);
 
-    /* round up to 1, 2, 5, times 10^N */
-    if(b > 10) delta = 20 * upow; // <-- May be this case can happen?
-    else if(b > 5) delta = 10 * upow;
-    else if(b > 2) delta = 5 * upow;
-    else if(b > 1) delta = 2 * upow;
-    else delta = upow;
+    /* round up to b = 1, 2, 5, times 10^p */
+    if(b > 10) {
+        // Maybe this case can't happen?
+        delta = 20 * upow;
+        p += 1;
+        upow = Math.pow(10, p);
+        b = 2;
+    } else if(b > 5) {
+        p += 1;
+        delta = upow = Math.pow(10, p);
+        b = 1;
+    } else if(b > 2) {
+        delta = (b = 5) * upow;
+    } else if(b > 1) {
+        delta = (b = 2) * upow;
+    } else {
+        delta = upow;
+        // b = 1
+    }
+
+    // The pixels per grid is greater than or equal to the requested
+    // pixPerGrid
+    let _pixPerGrid = delta*pixels/(max - min);
+
+    let size = Math.max(Math.abs(min),Math.abs(max));
+
+    let digits = Math.ceil(Math.log10(size/delta));
+    if(b > 1) ++digits;
+    if(digits === 0) digits = 1;
 
     // Now find the start
     //
@@ -54,11 +77,18 @@ function _GetGridSpacing(pixPerGrid, min, max, pixels/*width or height*/) {
     let start = Math.trunc((min-delta)/delta) * delta;
 
     // debug spew
-    //console.log('\n\n pixels=' + pixels + ' min=' + min + ' max=' + max + ' pixPerGrid=' + pixPerGrid + ' delta=' + delta + ' start=' + start);
-    //console.log('delta ~=' +  b + 'x10^' + p);
+    //console.log('\n\n pixels=' + pixels + ' min=' + min + 
+    //' max=' + max + ' pixPerGrid=' + pixPerGrid + ' delta=' +
+    //delta + ' start=' + start);
+    console.log('delta =' + delta + ' ~= ' + b + 'x10^' + p + ' digits=' + digits  );
     //console.log('calculated pixPerGrid=' + (pixels * delta /(max - min)));
 
-    return { start: start, delta: delta };
+    return { 
+        start: start,
+        delta: delta,
+        pixPerGrid: _pixPerGrid,
+        digits: digits
+    };
 }
 
 
@@ -68,6 +98,13 @@ function _GetGridSpacing(pixPerGrid, min, max, pixels/*width or height*/) {
 
 function Scope(period) {
 
+    /////////////////////////////// CONFIGURATION /////////////////////////
+    var majGridXWidth = 2.2;
+    var majGridYWidth = 2.2;
+    var majGridXColor = 'rgb(8,8,8)';
+    var majGridYColor = 'rgb(8,8,8)';
+    var gridFont = '20px serif';
+    ///////////////////////////////////////////////////////////////////////
 
     // This class object has 3 canvases, all the same size.
     //
@@ -88,7 +125,7 @@ function Scope(period) {
     // These are the min and max values across the whole canvas.  These
     // min and max values are not from value that are input, but are the
     // limiting values at the edges of the canvas.
-    var xMin = -10.5, xMax = 0.1;
+    var xMin = -100.52, xMax = -100.1;
     var yMin = -0.32, yMax = 1.0;
 
     assert(xMin < xMax);
@@ -109,7 +146,7 @@ function Scope(period) {
 
     // Minimum major Grid pixels per grid line.  Larger means the grid
     // lines have more pixels between them.
-    var majPixPerGrid = 50;
+    var majPixPerGrid = 100;
     var majPixPerGridX = majPixPerGrid; // for vertical grid lines
     var majPixPerGridY = majPixPerGrid; // for horizontal grid lines
 
@@ -119,12 +156,11 @@ function Scope(period) {
     //
     var xScale, yScale, xShift, yShift;
 
+    //////////////////////// 
     var majGridX = null;
     var majGridY = null;
-    var majGridXWidth = 2.2;
-    var majGridYWidth = 2.2;
-    var majGridXColor = 'rgb(8,8,8)';
-    var majGridYColor = 'rgb(8,8,8)';
+
+    bgCtx.font = gridFont;
 
 
     // Functions to convert from user x values to pixel coordinates.
@@ -145,8 +181,8 @@ function Scope(period) {
         majGridX = _GetGridSpacing(majPixPerGridX, xMin, xMax, w/*pixels*/);
         majGridY = _GetGridSpacing(majPixPerGridY, yMin, yMax, h/*pixels*/);
 
-        let w_1 = parseFloat(w-1);
-        let h_1 = parseFloat(h-1);
+        let w_1 = w-1;
+        let h_1 = h-1;
 
         xScale =  w_1/(xMax - xMin);
         xShift = w_1*xMin/(xMax - xMin);
@@ -154,28 +190,48 @@ function Scope(period) {
         yScale = -h_1/(yMax - yMin);
         yShift = -h_1*yMax/(yMax - yMin);
 
-        let n = 0;
 
         bgCtx.lineWidth = majGridXWidth;
         bgCtx.strokeStyle = majGridXColor;
-        let max = xPix(xMax) + majGridXWidth/2 + 1;
-        for(let x = xPix(majGridX.start); x <= max;
-            x = xPix(majGridX.start + (++n)*majGridX.delta)) {
+        bg.fillStyle = majGridXColor;
+        bgCtx.font = gridFont;
+        bg.fillStyle = majGridXColor;
+
+        let n = 0;
+        let max = xMax + majGridX.delta;
+
+        for(let x = majGridX.start; x <= max;
+            x = majGridX.start + (++n)*majGridX.delta) {
+            let xpix = xPix(x);
             bgCtx.beginPath();
-            bgCtx.moveTo(x, 0);
-            bgCtx.lineTo(x, h);
-            bgCtx.stroke();      
+            bgCtx.moveTo(xpix, 0);
+            bgCtx.lineTo(xpix, h);
+            bgCtx.stroke();
+            bgCtx.fillText(x.toPrecision(majGridX.digits),
+                    xpix+3,
+                    yPix(majGridY.start + 1*majGridY.delta) -
+                    majGridY.pixPerGrid/2 + 4);
+
+
         }
+
         bgCtx.lineWidth = majGridYWidth;
         bgCtx.strokeStyle = majGridYColor;
+        bg.fillStyle = majGridYColor;
+
         n = 0;
-        let min = yPix(yMax) - majGridYWidth/2 - 1;
-        for(let y = yPix(majGridY.start); y >= min;
-            y = yPix(majGridY.start + (++n)*majGridY.delta)) {
+        max = yMax + majGridY.delta;
+
+        for(let y = majGridY.start; y <= max;
+            y = majGridY.start + (++n)*majGridY.delta) {
+            let ypix = yPix(y);
             bgCtx.beginPath();
-            bgCtx.moveTo(0, y);
-            bgCtx.lineTo(w, y);
-            bgCtx.stroke();      
+            bgCtx.moveTo(0, ypix);
+            bgCtx.lineTo(w, ypix);
+            bgCtx.stroke();
+            bgCtx.fillText(y.toPrecision(majGridY.digits),
+                    xPix(majGridX.start + 1.5*majGridX.delta) - 10,
+                    ypix-4);
         }
      }
 
