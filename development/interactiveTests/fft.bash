80acdcdf4bf7492621a805536ee042c3bf58920f
Lance Arsenault <lanceman@vt.edu>
2019-05-24 22:21:59 -0400

    Saving Work

--- development/interactiveTests/fft.bash
+++ development/interactiveTests/fft.bash
@@ -3,11 +3,10 @@
 set -e
 cd $(dirname ${BASH_SOURCE[0]})
 
-source usrp_config
-
 crts_radio="../../bin/crts_radio"
+
+source usrp_config
 
 ./termRun
 
 ./termRun uhd_fft -f 915.0e6 -s 10.5e6 --args $USRP3
-
