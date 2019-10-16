85bb8fefd28e53dd7aec82cee6ddc42b1ea05208
Kalyani Gadgil <krightangle@gmail.com>
2019-10-16 15:01:04 -0400

    Hey Lance, default prefixes, uhd download file with shasum for v3.14.1.1, fm demod filter+test

---
+++ share/crts/plugins/Filters/liquidFMDemod.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <complex>
+
+#include "liquid.h"
+
+#include "crts/debug.h"
+#include "crts/Filter.hpp"
+#include "crts/crts.hpp"
+
+#define URL "http://liquidsdr.org/doc/freqmodem/"
+
+class LiquidFMDemod : public CRTSFilter
+{
+    public:
+        LiquidFMDemod(int argc, const char **argv);
+        ~LiquidFMDemod(void);
+        bool start(uint32_t numInChannels, uint32_t numOutChannels);
+        bool stop(uint32_t numInChannels, uint32_t numOutChannels);
+        void input(void *buffer, size_t len, uint32_t inChannelNum);
+
+    private:
+        freqdem fdem;
+        float kf;
+        std::complex<float> s;      //input FM signal
+        float y;                    //output demodulated bytes
+
+    public:
+        unsigned char *outputBuffer;
+        const size_t outBufferLen;
+        //bytes out at each write()
+        size_t len_out;
+};
+
+
+
+LiquidFMDemod::LiquidFMDemod(int argc, const char **argv):
+    fdem(0),
+    kf(0.1f),
+    outBufferLen(2*1024), len_out(0)
+{
+    fdem = freqdem_create(kf);
+    DSPEW();
+}
+
+bool LiquidFMDemod::start(uint32_t numInChannels, uint32_t numOutChannels)
+{
+    DSPEW();
+
+    if(isSource())
+    {
+        WARN("This filter cannot be a source filter.");
+        return true; // fail
+    }
+    if(numInChannels != 1)
+    {
+        WARN("Should have 1 input channel, got %" PRIu32,
+                numInChannels);
+        return true; // fail
+    }
+    if(numOutChannels != 1)
+    {
+        WARN("Should have 1 output channel, got %" PRIu32,
+            numOutChannels);
+        return true; // fail
+    }
+    fdem = freqdem_create(kf);
+    createOutputBuffer(outBufferLen);
+    return false; // success
+}
+
+
+bool LiquidFMDemod::stop(uint32_t numInChannels, uint32_t numOutChannels)
+{
+    DSPEW();
+    if(fdem)
+    {
+        freqdem_destroy(fdem);
+        fdem = 0;
+    }
+    return false; // success
+}
+
+
+void
+LiquidFMDemod::input(void *buffer, size_t len, uint32_t channelNum)
+{
+    DASSERT(buffer, "");
+    DASSERT(len, "");
+    len_out = 0;
+    outputBuffer = (unsigned char *) getOutputBuffer(0);
+    advanceInput(len-len%sizeof(std::complex<float>));
+    freqdem_demodulate(fdem, s, &y);
+
+}
+
+
+LiquidFMDemod::~LiquidFMDemod(void)
+{
+    DSPEW();
+}
+
+
+// Define the module loader stuff to make one of these class objects.
+CRTSFILTER_MAKE_MODULE(LiquidFMDemod)
