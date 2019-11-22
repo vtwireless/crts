36e20b05b2c925d985e7f5b6b0161decb22ffeaf
kgadgil <krightangle@gmail.com>
2019-11-22 14:06:44 -0500

    block demod instead of single byte, termRun while running fm_receive.sh

--- exampleScenarios/fm_receive.sh
+++ exampleScenarios/fm_receive.sh
@@ -3,16 +3,14 @@
 set -e
 cd $(dirname ${BASH_SOURCE[0]})
 
-USRP2="addr=192.168.40.206"
 
 crts_radio="../bin/crts_radio"
 
-exec $crts_radio\
+./termRun "$crts_radio\
      -f rx [ --uhd $USRP2 --freq 105.3 --rate 0.4 --gain 0 ]\
      -f liquidFMDemod\
      -f stdout\
-     #-C logger [ --file op.txt rx freq ]\ 
      -D |\
-    hexdump -v
+    hexdump -v"
 
 
