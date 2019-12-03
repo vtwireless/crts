5bc392216a68bf1dcff4f234d47c71e265393fe4
kgadgil <krightangle@gmail.com>
2019-12-03 16:43:44 -0500

    onlytx

--- exampleScenarios/onlyTx.bash
+++ exampleScenarios/onlyTx.bash
@@ -9,12 +9,12 @@ crts_radio="../bin/crts_radio"
 
 start=$SECONDS
 
-sudo tcpdump -i eth1 dst 192.168.40.203 -nn -B 8192 -XSs 0 -w nw.pcap &
+sudo tcpdump -i eth1 host 192.168.40.203 -nn -B 8192 -XSs 0 -w nw.pcap &
 exec cat /dev/urandom |\
  $crts_radio\
  -f stdin\
  -f liquidFrame\
- -f tx [ --uhd $USRP1 --freq 915.5 --rate 0.4 --gain 15 ]\
+ -f tx [ --uhd $USRP1 --freq 5000.0 --rate 0.4 --gain 15 ]\
  -C dualTx \
  -D
 duration=$(( SECONDS - start ))
